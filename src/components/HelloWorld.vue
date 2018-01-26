94de95d47a6e82278318ee1cd5ce58c97d3c2498
薛小康 <xuexk18973@hundsun.com>
2018-01-26 19:38:48 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+       <el-input-number v-model="num1" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        num1: 1
+      };
+    },
+    methods: {
+      handleChange(value) {
+        console.log(value);
+      }
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .test {
+    color: #333;
+    .p {
+      color: #666;
+    }
+  }
+</style>
